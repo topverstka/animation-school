8decebb599576b8a280a25cc7a55966451fd36d5
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-04-07 10:44:45 +0300

    Делает страницу работ студентов

--- src/js/ans_components/search.js
+++ src/js/ans_components/search.js
@@ -1,6 +1,8 @@
 const searchBoxes = document.querySelectorAll('.search');
 searchBoxes.forEach((search) => {
 	const input = search.querySelector('.search__input');
+	if (!input) return; 
+	
 	input.addEventListener("input", (e) => {
 		if (e.target.value.length > 0) {
 			search.classList.add('search--active');
